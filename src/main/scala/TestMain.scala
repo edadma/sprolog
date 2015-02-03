d58c59a5492fc16598d5b2078166caca69bda931
Edward A Maxedon, Sr <edadma@gmail.com>
2015-02-03 03:28:45 -0500

    add more tests

--- src/main/scala/TestMain.scala
+++ src/main/scala/TestMain.scala
@@ -4,44 +4,46 @@ package ca.hyperreal.sprolog
 object TestMain extends App
 {
 	val wam = new WAM
-// 	val p = Prolog.parseProgram( """
-// X = X.
+//  	val p = Prolog.parseProgram( """
+// 		X = X.
+// 		
+// 		man(adam).
+// 		man(peter).
+// 		man(rick).
+// 		man(paul).
 // 
-// man(adam).
-// man(peter).
-// man(rick).
-// man(paul).
+// 		woman(marry).
+// 		woman(eve).
 // 
-// woman(marry).
-// woman(eve).
+// 		parent(adam,peter). % means adam is parent of peter
+// 		parent(eve,peter).
+// 		parent(rick,paul).
+// 		parent(marry,paul).
 // 
-// parent(adam,peter). % means adam is parent of peter
-// parent(eve,peter).
-// parent(rick,paul).
-// parent(marry,paul).
-// 
-// father(F,C) :- man(F), parent(F,C).
-// mother(M,C) :- woman(M), parent(M,C).
-// 
-// """ )
-	val p = Prolog.parseProgram( """
-%X = X.
-
-reverse( List, Reversed ) :- reverse( List, [], Reversed ).
-
-reverse( [], Reversed, Reversed ).
-reverse( [Head|Tail], SoFar, Reversed ) :- reverse( Tail, [Head|SoFar], Reversed ).
-""" )
+// 		father(F,C) :- man(F), parent(F,C).
+// 		mother(M,C) :- woman(M), parent(M,C).
+// 		""" )
+ 	val p = Prolog.parseProgram( """
+		X = X.
+		
+		member( X, [X|_] ).
+		member( X, [_|T] ) :- member( X, T ).
+		
+		subset( [], _ ).
+		subset( [H|T], L ) :-
+			member( H, L ),
+			subset( T, L ).
+		""" )
 	val pc = Prolog.compileProgram( p )
 
 //	Prolog.listing( pc.code )
 	wam.program = pc
 	
-//  	val q = Prolog.parseQuery( "father( A, B ), B = paul." )
- 	val q = Prolog.parseQuery( "reverse( [a, b, c], L )." )
+  	val q = Prolog.parseQuery( "subset( [b, c], [d, a, c, b] )." )
 	val qc = Prolog.compileQuery( q )
-	
-//	Prolog.listing( qc.code )
+
+// 	println
+// 	Prolog.listing( qc.code )
 	
 	wam query qc
 }
