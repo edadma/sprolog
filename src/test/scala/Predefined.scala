889d336d301370b9b8d6de82b5e9ae15b72b6fb7
Edward A Maxedon, Sr <edadma@gmail.com>
2015-02-08 01:42:47 -0500

    add tests for if-then-else (-> ;) definition

--- src/test/scala/Predefined.scala
+++ src/test/scala/Predefined.scala
@@ -11,7 +11,9 @@ class Predefined extends FreeSpec with PropertyChecks with Matchers
 	"logic and control" in
 	{
 	val p = program( """
-		C -> T :- C, !, T.
+		If -> Then        :- If, !, Then.
+		If -> Then ; _    :- If, !, Then.
+		_  -> _    ; Else :- Else, !.
 		
 		F ; _ :- F.
 		_ ; A :- A.
@@ -38,5 +40,12 @@ class Predefined extends FreeSpec with PropertyChecks with Matchers
 		query( p, "legs(A, 6) -> write(insect(A))." ) shouldBe "insect(bee)A = bee"
 		query( p, """X \= 0 -> write('positive').""" ) shouldBe "no"
 		query( p, "fail -> (true ; true)." ) shouldBe "no"
+		
+		// -> ; 108
+		query( p, "X = 0 -> write('null'); write('positive')." ) shouldBe "nullX = 0"
+		query( p, "X = 1, (X = 0 -> write('null'); write('positive'))." ) shouldBe "positiveX = 1"
+		query( p, "((!, X = 1, fail) -> true; fail); X = 2." ) shouldBe "X = 2"
+		query( p, "fail -> true ; true." ) shouldBe "yes"
+		query( p, "(!, X = 1, fail) -> true; fail." ) shouldBe "no"
 	}
 }
